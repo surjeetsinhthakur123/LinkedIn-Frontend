c0cfeb8b3cb454fcea850e8f8c4b56d1fcbaf2d9
Surjeetsinh Nandkumar Thakur <tsurjeet143@gmail.com>
2025-10-29 11:18:30 +0530

    Update axios base URL to new endpoint

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://linkedin-backend-161k.onrender.com/api/v1",
+  baseURL: "https://surjeet-backend.onrender.com/api/v1", 
   withCredentials: true,
 });
